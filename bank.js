c50fed4e6b59068f5cd8acadf2c727c0f10caf75
Arya <iaryasinha2001@gmail.com>
2021-03-19 14:48:56 +0530

    Add gitignore, Procfile and update ports

--- bank.js
+++ bank.js
@@ -158,8 +158,13 @@ app.post("/money-transfer",function(req,res){
  res.redirect("/money-transfer");
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
 
-app.listen("3000", function(){
-  console.log("Server started at port 3000");
+app.listen(port, function(){
+  console.log("Server has started Successfully!!");
 });
